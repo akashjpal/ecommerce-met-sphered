4fdcde758968f95ba4d7bac0a96e405245165e7d
palgayatrij786 <palgayatrij786@gmail.com>
2024-05-28 13:43:38 +0000

    Updated existing filw

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="header-section flex flex-row justify-evenly">
+      <div className="header-section flex flex-row ">
          <Image
           src="/images/Preview.png"
           alt="This is a Logo"
@@ -17,6 +17,9 @@ export default function Home() {
         </div>
         <div>
           <button className="px-5 py-1 bg-blue-500 hover:bg-blue-400 text-white font-semibold rounded-md">Login</button>
+        </div>
+        <div>
+          <button>Cart</button>
         </div>
       </div>
       <div className="main-section">
