fab73e841ce322e01eca2c3f597b4a32ea0d447e
palgayatrij786 <palgayatrij786@gmail.com>
2024-05-28 13:40:33 +0000

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="header-section flex flex-row justify-evenly">
+         <Image
+          src="/images/Preview.png"
+          alt="This is a Logo"
+          width={50}
+          height={30}
+        />
+        <div>
+          <form>
+            <input type="text" placeholder="Search" />
+          </form>
+        </div>
+        <div>
+          <button className="px-5 py-1 bg-blue-500 hover:bg-blue-400 text-white font-semibold rounded-md">Login</button>
+        </div>
+      </div>
+      <div className="main-section">
+
+      </div>
+      <div className="feature-section">
+
+      </div>
+      <div className="testinomial-section">
+
+      </div>
+      <div className="footer-section">
+
+      </div>
+    </div>
+  );
+}
